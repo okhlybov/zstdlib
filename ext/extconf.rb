9bbe7c7eb7d75f64ec0c4e79d98021a8bb550324
fougas <fougas@localhost>
2019-03-26 17:40:15 +0500

    Added zstdlib build system.

---
+++ ext/extconf.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'mkmf'
+require 'rbconfig'
+require 'fileutils'
+
+include RbConfig
+include FileUtils
+
+ZSTD_VERSION = '1.3.8'
+RB_VERSION = CONFIG['RUBY_API_VERSION']
+
+zlib = "zlib-#{RB_VERSION}"
+zstd = "zstd-#{ZSTD_VERSION}/lib"
+zlibwrapper = "zstd-#{ZSTD_VERSION}/zlibWrapper"
+
+File.open('zstdlib.c', 'w') do |file|
+  file << File.read("#{zlib}/zlib.c").
+    gsub(/Init_zlib/, 'Init_zstdlib').
+    gsub(/rb_define_module.*/, 'rb_define_module("Zstdlib");').
+    gsub(/<zlib.h>/, '<zstd_zlibwrapper.h>')
+end
+
+$CFLAGS += ' -O3'
+$CPPFLAGS += " -I#{zlibwrapper} -DZWRAP_USE_ZSTD=1 -DGZIP_SUPPORT=0"
+
+load "#{zlib}/extconf.rb"
+
+mk = File.read('Makefile').
+  gsub(/^\s*(LIBS\s*=.*)/, '\1 -lzstd -lzlibwrapper').
+  gsub(/^\s*(TARGET(_NAME)?\s*=).*/, '\1 zstdlib').
+  gsub(/^(\s*clean\s*:.*)/, '\1 clean-mk')
+
+File.open('Makefile', 'wt') do |file|
+  file << mk
+  file <<
+%~
+export CFLAGS CPPFLAGS
+$(DLLIB) : libzstd.a libzlibwrapper.a
+libzstd.a :
+\tSRCDIR=#{zstd} $(MAKE) -f zstd.mk
+libzlibwrapper.a :
+\tSRCDIR=#{zlibwrapper} $(MAKE) -f zlibwrapper.mk
+clean-mk:
+\tSRCDIR=#{zstd} $(MAKE) -f zstd.mk clean
+\tSRCDIR=#{zlibwrapper} $(MAKE) -f zlibwrapper.mk clean
+~
+end
