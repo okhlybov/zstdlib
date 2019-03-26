d9844a9d98968c066941a7cada1eb4c53de75697
fougas <fougas@localhost>
2019-03-26 14:27:44 +0500

    Fixed line endings.

--- ext/zstd.rb
+++ ext/zstd.rb
@@ -5,10 +5,9 @@
 require 'fileutils'
 include FileUtils
 
-
 cd Dir["zstd-*"].first do
   dirs = Dir.glob("**/", File::FNM_DOTMATCH).select {|d| File.directory?(d)}
-  dirs -= dirs.select {|d| Regexp.new("^(lib/$|lib/(common|compress|decompress)|zlibWrapper)").match(d) && !/example/.match(d)}
+  dirs -= dirs.select {|d| Regexp.new("^(lib/$|lib/(common|compress|decompress)|zlibWrapper/$)").match(d) && !/example/.match(d)}
   dirs.each {|d| remove_dir(d, true)}
   files = Dir.glob("**/*", File::FNM_DOTMATCH).reject {|f| File.directory?(f) || /\.[ch]$/ =~ f}
   files.each {|f| rm_f(f)}
