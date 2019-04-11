0032a705185b8b587f91f27768d2954de8d48ba2
Oleg A. Khlybov <fougas@mail.ru>
2019-04-11 17:01:30 +0500

    Made fixes to release script.

--- release.sh
+++ release.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+hg id | grep + > /dev/null && { echo "Uncommitted changes found. Commit things first!"; exit; }
+
 bundle exec rake clobber build fat
+
+gem push pkg/*.gem
+
+hg tag "zstdlib-`ruby -e 'puts Gem::Specification.load(%~zstdlib.gemspec~).version'`-release"
 
 #
